b2a00c99ec6a55e7b025a61a6cf1c2ff5abf9088
Damian Piazza <83801199+XkaunanX@users.noreply.github.com>
2024-11-16 20:15:37 -0300

    Add files via upload

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Prueba de funcionamiento de C: ¡Todo está operando correctamente!\n");
+
+    int num;
+    printf("Por favor ingrese un número entero: ");
+    scanf("%d", &num);
+
+    printf("El numero ingresado es: %d\n", num);
+
+    return 0;
+}
